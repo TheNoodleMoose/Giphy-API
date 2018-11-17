b4eed897c15330cd4b6dffc5bc39c52d7f9f1186
TheNoodleMoose <cjh605@gmail.com>
2018-11-17 17:24:05 -0500

    Got buttons to be created when you search, site is almost complete

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,8 +1,37 @@
+var submittedButtons = [];
 
+$(".search-btn").on("click", function(event){
+    event.preventDefault();
+    var search = $(".searchResult").val().trim();
+    console.log(search)
+    var btn = $("<button>")
+    btn.addClass("gifBtn newBtn m-1 btn-hover color-4")
+    btn.text(search)
+    btn.attr("data", search)
+    $(".buttonsGoHere").append(btn)
+    
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=oYO3qWVRQJ8coSdr6nEQg4APTLmHHhCT&limit=10";
 
-$("button").on("click", function () {
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response)
+        var results = response.data;
+        for (i = 0; i < results.length; i++) {
+            var images = $("<img>");
+            images.addClass("gifs")
+            images.attr("src", results[i].images.fixed_height.url);
+            $("#trendingSection").prepend(images)
+        }
+
+    })
+})
+
+$(".gifBtn").on("click", function (event) {
+    event.preventDefault();
     var selection = $(this).attr("data")
-
+    console.log("This is working")
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + selection + "&api_key=oYO3qWVRQJ8coSdr6nEQg4APTLmHHhCT&limit=10";
 
     $.ajax({
@@ -20,3 +49,26 @@ $("button").on("click", function () {
 
     })
 })
+
+$(document).on("click", ".newBtn", function(){
+    event.preventDefault();
+    var selection = $(this).attr("data")
+    console.log("This is working")
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + selection + "&api_key=oYO3qWVRQJ8coSdr6nEQg4APTLmHHhCT&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response)
+        var results = response.data;
+        for (i = 0; i < results.length; i++) {
+            var images = $("<img>");
+            images.addClass("gifs")
+            images.attr("src", results[i].images.fixed_height.url);
+            $("#trendingSection").prepend(images)
+        }
+
+    })
+  })
+
