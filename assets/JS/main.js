30eed8861acca59af5bf3371598d6621c9edab0c
TheNoodleMoose <cjh605@gmail.com>
2018-11-17 22:06:14 -0500

    Got gif pausing and unpausing finished

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,5 +1,9 @@
+// array that holds values for the button classes to style new buttons 
 var colorOptions = ["color-2","color-3","color-4", "color-5","color-6","color-7","color-8", "color-9", "color-10", "color-11"];
+// array that holds the values for the gif classes that style the bottom
 var gifBottom = ["gif-bottom-red", "gif-bottom-blue", "gif-bottom-purple", "gif-bottom-green"]
+
+//This is runs the ajax and button generating when the search button is hit
 $(".search-btn").on("click", function(event){
     event.preventDefault();
     var randomColor = colorOptions[Math.floor(Math.random() * colorOptions.length)];
@@ -23,7 +27,12 @@ $(".search-btn").on("click", function(event){
         for (i = 0; i < results.length; i++) {
             var images = $("<img>");
             images.addClass("gifs")
-            images.attr("src", results[i].images.fixed_height.url);
+            images.addClass(randomColor)
+            //After graded rememeber to remove the the still image feature because its not necessary, src will just go back to fix_height.url
+            images.attr("data-state", "still")
+            images.attr("data-still", results[i].images.fixed_height_still.url)
+            images.attr("data-animate", results[i].images.fixed_height.url)
+            images.attr("src", images.attr("data-still"));
             $("#trendingSection").prepend(images)
         }
 
@@ -47,11 +56,31 @@ $(".gifBtn").on("click", function (event) {
             var images = $("<img>");
             images.addClass("gifs")
             images.addClass(randomColor)
-            images.attr("src", results[i].images.fixed_height.url);
+            //After graded rememeber to remove the the still image feature because its not necessary, src will just go back to fix_height.url
+            images.attr("data-state", "still")
+            images.attr("data-still", results[i].images.fixed_height_still.url)
+            images.attr("data-animate", results[i].images.fixed_height.url)
+            images.attr("src", images.attr("data-still"));
             $("#trendingSection").prepend(images)
         }
 
     })
+})
+
+
+//Remove this because pausing is not needed after grading
+$(document).on("click", ".gifs" , function() {
+    var state = $(this).attr("data-state")
+
+    if(state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"))
+        $(this).attr("data-state", "animate")
+    }
+
+    if(state === "animate") {
+        $(this).attr("src", $(this).attr("data-still"))
+        $(this).attr("data-state", "still")
+    }
 })
 
 $(document).on("click", ".newBtn", function(){
